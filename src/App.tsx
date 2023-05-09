79e17db135a80e7dd95c450e410fd93f3d49ad8d
WebZerefose <anything.codable@gmail.com>
2023-05-09 13:42:16 -0300

    feat: create modules login

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { SafeAreaView, StyleSheet } from 'react-native'
+import React from 'react'
+import LoginScreen from './modules/login'
+
+const App = () => {
+	return (
+		<SafeAreaView>
+			<LoginScreen />
+		</SafeAreaView>
+	)
+}
+
+export default App
+
+const styles = StyleSheet.create({})
